b63f7611bbf8052fb9e376dbeab583aad8b149da
kiryuha <ksamozvon@gmail.com>
2024-06-05 22:04:00 +0300

    Update pacm8installer.sh

--- pacm8installer.sh
+++ pacm8installer.sh
@@ -24,12 +24,13 @@ EOF
 
 #~INSTALL GUM~
 echo " "
-if ! command -v gum &> /dev/null
-then
-    gum style --foreground 2 "gum not found, installing gum..."
+if ! command -v gum &> /dev/null; then
+    echo "gum not found, installing gum..."
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
     sudo apt update && sudo apt install -y gum
-    if ! command -v gum &> /dev/null
-    then
+    if ! command -v gum &> /dev/null; then
         gum style --foreground 1 "Failed to install gum. Please install it manually."
         exit 1
     fi
